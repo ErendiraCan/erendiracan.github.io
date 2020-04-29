dbc689b12d1ffd616cc3689e536c8ee72507c76d
ErendiraCan <can19004@byui.edu>
2020-04-28 19:35:19 -0500

    first javascript

---
+++ CIT160/robbietherobot.js
@@ -0,0 +1,14 @@
+moveForward();
+moveForward();
+moveForward();
+turnRight();
+moveForward();
+moveForward();
+moveForward();
+moveForward();
+turnLeft();
+moveForward();
+moveForward();
+moveForward();
+turnRight();
+print("Hi everyone I'm Robbie the Robot");
