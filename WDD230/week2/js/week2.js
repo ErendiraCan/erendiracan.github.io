fb9223d0ef1844ce2045b52adb544e8afd21dac8
ErendiraCan <can19004@byui.edu>
2020-09-25 22:31:50 -0500

    Ready

--- WDD230/week2/js/week2.js
+++ WDD230/week2/js/week2.js
@@ -1,3 +1,5 @@
-document.getElementById("lastupdated").innerHTML = document.lastModified;
+window.onload = function date () {
+document.getElementById("updated").innerHTML = document.lastModified;
 
 document.getElementById("currentyear").textContent = new Date().getFullYear();
+}
