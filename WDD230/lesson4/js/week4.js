8b3835fbb1a8a016232eff921777a1c7e980b068
ErendiraCan <can19004@byui.edu>
2020-10-09 18:09:12 -0500

    ready

--- WDD230/lesson4/js/week4.js
+++ WDD230/lesson4/js/week4.js
@@ -1,17 +1,11 @@
+// Hamburger Button
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
-/*** Programming Notes **************************************
-  Arrow Functions - es6 syntactically compact alternative to a regular function expression
-  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
-  or https://www.w3schools.com/js/js_arrow_function.asp
+//Current date
+const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
+document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options)
 
-  classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
-  */
-
-
-    const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
-    document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options)
