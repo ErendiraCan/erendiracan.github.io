0227b927b63975498ed66afbe9fc73df679f3d0f
ErendiraCan <can19004@byui.edu>
2020-10-09 20:31:43 -0500

    Ready JS

--- WDD230/lesson4/js/week4.js
+++ WDD230/lesson4/js/week4.js
@@ -7,5 +7,5 @@ window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('
 
 //Current date
 const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
-document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options)
+document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options);
 
