61202a784ab8456bd4e26f3fc444f66a621ea3fe
ErendiraCan <can19004@byui.edu>
2020-10-13 21:32:56 -0500

    Still working in week5

---
+++ WDD230/lesson5/js/week5.js
@@ -0,0 +1,11 @@
+// Hamburger Button
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+//Current date
+const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
+document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options);
+
