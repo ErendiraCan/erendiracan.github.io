1cef8e6e06704544c5735ca7a3a397540e0d45ce
erendiracan.github.io <can19004@byui.edu>
2020-10-23 14:17:27 -0500

    adding week6 to the index

--- WDD230/lesson6/js/week6.js
+++ WDD230/lesson6/js/week6.js
@@ -18,3 +18,6 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 //Current date
 const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
 document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options);
+
+//Web Font Loader
+WebFont.load({ google: {families: ['Oswald', 'Montserrat']}});
