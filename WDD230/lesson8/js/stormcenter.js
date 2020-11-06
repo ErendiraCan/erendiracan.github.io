47566a91d2535ec67b1fbd7792ef096386616eb2
erendiracan <can19004@byui.edu>
2020-11-06 12:27:15 -0600

    adding images and css

---
+++ WDD230/lesson8/js/stormcenter.js
@@ -0,0 +1,28 @@
+// Message
+const d = new Date();
+let message = document.querySelector(".message");
+
+if (d.getDay() == 5) {
+    message.style.display = "block";
+};
+
+// Hamburger Button
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+//Current Year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//Current date
+const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
+document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options);
+
+//Web Font Loader
+WebFont.load({ google: {families: ['Oswald', 'Montserrat', ]}});
+
+//Form
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
