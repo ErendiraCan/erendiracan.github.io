add49deec3eb8786b7f02011ec36fb5fd0999791
erendiracan <can19004@byui.edu>
2021-01-04 16:26:31 -0600

    Ready index

---
+++ WDD230/js/index.js
@@ -0,0 +1,5 @@
+//Getting the Year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//Getting the last modified date
+document.getElementById("update").innerHTML = document.lastModified;
