d10e4b86444c09b3e4384701ef030579c95d4105
erendiracan <can19004@byui.edu>
2021-01-11 14:08:01 -0600

    adding the website planning file

---
+++ WDD230/lesson2/js/index.js
@@ -0,0 +1,9 @@
+window.onload = function date () {
+
+    //Getting the Year
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+    
+    //Getting the last modified date
+    document.getElementById("update").innerHTML = document.lastModified;
+    
+    }
