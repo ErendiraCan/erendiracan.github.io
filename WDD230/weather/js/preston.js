978ee8ec437169521acd303ef3bd7dceee0c7656
erendiracan <can19004@byui.edu>
2021-01-15 15:34:37 -0600

    finishing the preston 1st file

---
+++ WDD230/weather/js/preston.js
@@ -0,0 +1,12 @@
+//Getting the current date
+const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
+document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options);
+
+//Making the ham buttom works <<credits: https://codepen.io/blazzard-jason/pen/gmGxaj/>>
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
