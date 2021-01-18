986609d5da6f56887c6c37e8af9531f29f7656d7
erendiracan <can19004@byui.edu>
2021-01-18 14:29:10 -0600

    adding images

--- WDD230/weather/js/preston.js
+++ WDD230/weather/js/preston.js
@@ -2,11 +2,14 @@
 const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
 document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options);
 
-//Making the ham buttom works <<credits: https://codepen.io/blazzard-jason/pen/gmGxaj/>>
+//Getting the Year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//Making the ham button works <<credits: https://codepen.io/blazzard-jason/pen/gmGxaj/>>
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
-// To solve the mid resizing issue with responsive class on
+// To solve the mid resizing issue with responsive class on (button section)
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
