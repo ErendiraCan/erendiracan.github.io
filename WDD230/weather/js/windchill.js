770d0dcbe4074ba91f1d939cd3a76130182f3449
erendiracan <can19004@byui.edu>
2021-01-21 13:02:36 -0600

    ready preston js and images

--- WDD230/weather/js/windchill.js
+++ WDD230/weather/js/windchill.js
@@ -2,19 +2,17 @@
 
 // 1 Getting the variables values: Input
 var temp = document.getElementById("temp").innerHTML;
-console.log(temp);
 var windspeed = document.getElementById("speed").innerHTML;
-console.log(windspeed);
 
 // 2 Formula to calculate the wind chill
-var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16));
 
 // 3 Round the output value
 var windChill= Math.round(windChill);
 
 // 4 Input requirements: Windchill temperature is below 50 °F and wind speeds above 3.0 mph
-if (temp <= 50 && windSpeed >= 3) {
-    document.getElementById("windchill").innerHTML = windChill;
+if (temp <= 50 && windspeed >= 3) {
+    document.getElementById("windChill").innerHTML = windChill;
 } else {
-    document.getElementById("windchill").innerHTML = "N/A";
+    document.getElementById("windChill").innerHTML = "N/A";
 }
