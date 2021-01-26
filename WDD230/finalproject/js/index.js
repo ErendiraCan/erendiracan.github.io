08f49cae6cd010fa4a0a537330dff59738b6cee8
erendiracan <can19004@byui.edu>
2021-01-26 16:44:43 -0600

    changing stuff

--- WDD230/finalproject/js/index.js
+++ WDD230/finalproject/js/index.js
@@ -1,5 +1,8 @@
-// Hamburger Button
+//---------------Making the ham button works <<credits: https://codepen.io/blazzard-jason/pen/gmGxaj/>>
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+//To solve the mid resizing issue with responsive class on (button section)
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
