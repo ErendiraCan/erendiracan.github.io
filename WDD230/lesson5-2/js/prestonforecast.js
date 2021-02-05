f4d9eebdd73f3d075a4b1fc21b71f90439e470de
erendiracan <can19004@byui.edu>
2021-02-05 17:42:29 -0600

    fixing the js file

--- WDD230/lesson5-2/js/prestonforecast.js
+++ WDD230/lesson5-2/js/prestonforecast.js
@@ -11,6 +11,7 @@ fetch(forecast)
 
 const days = jsObject.list;
 for (let i = 0; i < days.length; i++) {
+  if (days[i].dt_txt.includes('18:00:00')){
 //days[i].dt_txt == '2021-02' + new Date().getDay() + '18:00:00';
 
   //Creating the HTML elements
@@ -20,10 +21,10 @@ for (let i = 0; i < days.length; i++) {
   let temp = document.createElement('span');
 
   //Adding data to the HTML elements
-  day.textContent = new Date().toLocaleDateString('en-US', {weekday: 'long'});
+  day.textContent = new Date(days[i].dt_txt).toLocaleDateString('en-US', {weekday: 'long'});
   icon.setAttribute('src', 'https://openweathermap.org/img/wn/' + days[i].weather[0].icon + '@2x.png');
   icon.setAttribute('alt', 'Icon: ' + days[i].weather[0].description);
-  temp.textContent = days[i].main.temp;
+  temp.textContent = days[i].main.temp + '°F';
 
   //The appendChild() method: It appends the new items created to the HTML elements
   //https://www.javascripttutorial.net/javascript-dom/javascript-appendchild/
@@ -33,6 +34,7 @@ for (let i = 0; i < days.length; i++) {
 
   //Calling the JS in the HTML
   document.querySelector('div.forecastbox').appendChild(box);
+  }
 }
 
 });
