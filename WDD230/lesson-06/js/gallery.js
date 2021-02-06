b82e7c2fafd8a0879e8888c8dc86f2fb1bb569f7
erendiracan <can19004@byui.edu>
2021-02-06 00:17:33 -0600

    adding js files

---
+++ WDD230/lesson-06/js/gallery.js
@@ -0,0 +1,37 @@
+//Code based from the Brother's Blazzard demonstration video https://video.byui.edu/media/t/0_0tixbudg
+
+//Get all images with data-src attribute
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+//Optional parameters being set for the IntersectionalObserver
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+//First check to see if the intersection Observer is supported
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+//Load all the images if not supported
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
---
+++ WDD230/lesson-06/js/weather.js
@@ -0,0 +1,19 @@
+//Selecting city
+if (document.getElementById("preston")) {
+    apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=49a941896ac322f17786b22ffb8b1fec&units=imperial';
+}
+
+//Usign fetch to feed the argument
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+//Calling the JS in the <span> tag in the HTML to print the current data
+document.getElementById('description').textContent = jsObject.weather[0].description;
+document.getElementById('temp').textContent = jsObject.main.temp;
+document.getElementById('humidity').textContent = jsObject.main.humidity;
+document.getElementById('speed').textContent = jsObject.wind.speed;
+
+getWindChill();
+});
