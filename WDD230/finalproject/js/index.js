affb7ae187ae15d3bef04f4dcb4e988de113a31c
erendiracan <can19004@byui.edu>
2021-02-08 01:12:09 -0600

    Adding more stuff to files

--- WDD230/finalproject/js/index.js
+++ WDD230/finalproject/js/index.js
@@ -6,3 +6,31 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 //To solve the mid resizing issue with responsive class on (button section)
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+//--------------------Slider Code
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
