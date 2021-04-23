80dd4f76cd32ada5439d6ec7b183a1988dedad72
erendiracan <can19004@byui.edu>
2021-04-23 17:43:14 -0500

    finishing week1 notes file

--- WDD330/js/main.js
+++ WDD330/js/main.js
@@ -1,19 +1,22 @@
+//Week links array
 const links = [
     {
-      label: "Week1 notes",
+      label: "Week1: Notes",
       url: "week1/index.html"
     }
   ]
-
+//Creating the ol tag element
 var list = document.getElementsByTagName('ol');
   
-// and append it to the list
+//Creating the li and a elements
   links.forEach(function (link) {
 	var li = document.createElement('li');
   var a = document.createElement('a');
   a.text = link.label;
+  //In case we want to divide the link from the label text
 	//li.textContent = link.label;
   a.href = link.url;
+//Making the elements being created inside ol and a inside il
 	list[0].appendChild(li);
   li.appendChild(a);
 });
