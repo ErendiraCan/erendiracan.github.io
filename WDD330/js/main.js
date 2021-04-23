e8b7a8efb83dd96115907949b17c589c40539669
erendiracan <can19004@byui.edu>
2021-04-22 22:40:15 -0500

    fixing js

--- WDD330/js/main.js
+++ WDD330/js/main.js
@@ -7,18 +7,21 @@ const links = [
 
 var list = document.getElementsByTagName('ol');
   
-// Create a list item for each wizard
 // and append it to the list
   links.forEach(function (link) {
 	var li = document.createElement('li');
-	li.textContent = link;
-	list.appendChild(li);
+  var a = document.createElement('a');
+  a.text = link.label;
+	//li.textContent = link.label;
+  a.href = link.url;
+	list[0].appendChild(li);
+  li.appendChild(a);
 });
 
 // Log
 console.log(list);
 
 // Inject into the DOM
-var app = document.querySelector('#weeklink');
-app.appendChild(list);
+//var app = document.querySelector('#weeklink');
+//app.appendChild(list);
 
